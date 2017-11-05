f4735984bf242d58386b932862c79dda1e3028ed
Bobby Hines <bobby@conflabs.com>
2017-11-05 00:01:57 -0700

    Update wapper.sh

--- wapper.sh
+++ wapper.sh
@@ -1,23 +1,55 @@
 #!/usr/bin/env bash
+# That first line is called a Shebang line. It lets the system know to 
+# pass these commands through the bash program
 
-dialog --title 'Wappalyzer' \
-         --backtitle 'Conflabs Dev Team' \
+dialog --title 'wapper' \
+         --backtitle 'https://bobbyahines.github.io/wapper/' \
          --no-cancel \
          --inputbox "Enter a URL:" 8 40 \
          https:// \
          2>url.$$
+         
+# After the shebang  line, dialog is called up as an input box requesting a URL.
+# --backtitle prints a string in the upper left corner of the widget's viewport.
+# --no-cancel removes the cancel button from the dialog widget.
+# --input makes the input box on the widget. The quoted text gets displayed like
+# a label on an html form. 8 is a height unit and 40 is a width unit
+# https:// is an input option for the --input tag that acts like an html form's
+# placeholder option.
+# 2>url.$$ sends Dialog's stderr stream to a file called url.$$. The $$ gets 
+# converted by the system into a random number.
 
 URL=$(<url.$$)
 
+# A variable called URL is instantiated with the contents of the url.$$ file. 
+# $() is a bash method for running a sort of  shell in shell command; most
+# anything you'd run on the command line inside of the parentheses. In this 
+# case, the < symbol indicates the URL variable will be populated by the contents
+# of the url.$$ file.
+
 rm -f url.$$
+
+# Now that the URL is store in a local variable, we can get rid of the temp file.
 
 DATETIME=$(date +%Y%m%d%H%M)
 
+# Next, a variable called DATETIME is instantiated and populate with a string-formatted datetimestamp.
+
 docker run --rm wappalyzer/cli $URL 1>>log-${URL#????????}-$DATETIME.txt
+
+# Docker is then ran using the --rm tag to make the serving container ephemeral. 
+# The `$URL` variable is given as a target, and the stdout is directed to a file named `log-url.domain.com-YYYYMMDDHHMM.txt`.
 
 RESULT=`cat log-${URL#????????}-$DATETIME.txt`
 
+# A variable called RESULT is instantiated and populated with the contents of the log file. The ticks ``
+# wrapping the cat command are a command substitution method that does the same thing as $().
+
 dialog --title "Wappalyzer" \
         --msgbox "$RESULT" 30 60
+        
+# A new dialog message box opens, and reads back RESULT variable into a message box.
 
 chown $USER:$USER log-${URL#????????}-$DATETIME.txt
+
+# On exit, the text file is preserved as a log file containing the json array, and permissions are modded to the running user.
